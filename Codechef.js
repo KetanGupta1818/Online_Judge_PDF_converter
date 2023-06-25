cedbaa61c8910cf7371f4a719e2a98d7eae1b65a
Ketan Gupta <84444599+KetanGupta1818@users.noreply.github.com>
2023-06-25 21:51:19 +0530

    Update Codechef.js

--- Codechef.js
+++ Codechef.js
@@ -1,11 +1,11 @@
 document.querySelectorAll('a').forEach(el => el.remove())
-var hh = document.getElementById('ember251').children[0].children[1].innerHTML
+var hh = document.getElementById('ember251').children[0].children[1].children[0].children[0].children[0].innerHTML
 var header_string = ""
 for (var i = 1; i < hh.length; i++) {
     if (hh.charAt(i) == '\n') break;
     header_string = header_string + hh.charAt(i);
 }
-var v1 = document.getElementById('ember-root').children[0].children[2].children[0].children[2].children[0]
+var v1 = document.getElementById('ember251').children[0].children[2].children[0]
 var arr = v1.getElementsByClassName("MathJax")
 for (var i = 0; i < arr.length; i++) {
     arr[i].style.fontSize = "0.7em";
